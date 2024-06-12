0f5667416eef35a901740f3ad26834095d66c185
CrockFrey <crockfrey@gmail.com>
2024-06-12 11:33:56 +0300

    push

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,7 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price * deliveryFee;
+    const totalPrice = price + deliveryFee;
     
-    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-    return message;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,11 @@
 function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
 
-    return contentWidth;
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+
+    return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
